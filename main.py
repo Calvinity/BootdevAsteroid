bf29ba4d77028eb82de32ca604c34929435fa3d1
Calvinity <per.mellbin@gmail.com>
2025-06-06 23:01:54 +0200

    done, for now

--- main.py
+++ main.py
@@ -2,7 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from circleshape import *
+from shot import *
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+Asteroid.containers = (updatable, drawable, asteroids)
+AsteroidField.containers = (updatable,)
+Shot.containers = (updatable, drawable, shots)
 
 def main():
     pygame.init()
@@ -10,13 +26,33 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    print("Asteroid destroyed!")
+                    asteroid.split()
+                    shot.kill()
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        delta_time_ms = clock.tick(60)
+        dt = delta_time_ms / 1000.0
+
 
 
 
